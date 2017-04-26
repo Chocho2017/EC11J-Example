8f2aebbc3e5691e4f69b592cb5b34fc90387962c
dfrobot-andy <andy.zhou@dfrbot.com>
2017-04-26 10:46:29 +0800

    v1.0

---
+++ DFRobot_EC11/DFRobot_EC11.ino
@@ -0,0 +1,65 @@
+int encoderPinA = 2;
+int encoderPinB = 3;
+int buttonPin = 4;
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
+
+long readEncoderValue(void){
+    return encoderValue/4;
+}
+
+boolean isButtonPushDown(void){
+  if(!digitalRead(buttonPin)){
+    delay(5);
+    if(!digitalRead(buttonPin))
+      return true;
+  }
+  return false;
+}
+
+void setup() {
+  Serial.begin (9600);
+
+  pinMode(encoderPinA, INPUT); 
+  pinMode(encoderPinB, INPUT);
+  pinMode(buttonPin, INPUT);
+
+  digitalWrite(encoderPinA, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPinB, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
+  attachInterrupt(0, updateEncoder, CHANGE); 
+  attachInterrupt(1, updateEncoder, CHANGE);
+
+}
+
+void loop(){ 
+  //Do stuff here
+
+  if(isButtonPushDown()){
+    Serial.println("you push button down!!!");
+  }
+  Serial.println(readEncoderValue());
+  delay(50); //just here to slow down the output, and show it will work  even during a delay
+}
+
+
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+
+  lastEncoded = encoded; //store this value for next time
+}
